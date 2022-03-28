92bfb7c23ef7c26c1b14f01d50bd05ec9af62d47
Surakattula Rohith <srohith.charan@gmail.com>
2022-03-28 10:14:15 +0530

    28_03 09_00

---
+++ lecture38.cpp
@@ -0,0 +1,131 @@
+#include <bits/stdc++.h>
+//#include<ext/pb_ds/assoc_container.hpp>
+//#include<ext/pb_ds/tree_policy.hpp>
+//#include <ext/pb_ds/trie_policy.hpp>
+// using namespace __gnu_pbds;
+using namespace std;
+#define ll long long int
+#define ld long double
+#define mod 1000000007
+#define inf 1e18
+#define nl "\n"
+#define pb push_back
+#define vi vector<ll>
+#define vs vector<string>
+#define pii pair<ll, ll>
+#define ump unordered_map
+#define mp make_pair
+#define pq_max priority_queue<ll>
+#define pq_min priority_queue<ll, vi, greater<ll>>
+#define all(n) n.begin(), n.end()
+#define ff first
+#define ss second
+#define mid(l, r) (l + (r - l) / 2)
+#define bitc(n) __builtin_popcount(n)
+#define loop(i, a, b) for (int i = (a); i <= (b); i++)
+#define looprev(i, a, b) for (int i = (a); i >= (b); i--)
+#define iter(container, it) for (__typeof(container.begin()) it = container.begin(); it != container.end(); it++)
+#define log(args...)                             \
+    {                                            \
+        string _s = #args;                       \
+        replace(_s.begin(), _s.end(), ',', ' '); \
+        stringstream _ss(_s);                    \
+        istream_iterator<string> _it(_ss);       \
+        err(_it, args);                          \
+    }
+#define logarr(arr, a, b)            \
+    for (int z = (a); z <= (b); z++) \
+        cout << (arr[z]) << " ";     \
+    cout << endl;
+template <typename T>
+T gcd(T a, T b)
+{
+    if (a % b)
+        return gcd(b, a % b);
+    return b;
+}
+template <typename T>
+T lcm(T a, T b) { return (a * (b / gcd(a, b))); }
+vs tokenizer(string str, char ch)
+{
+    std::istringstream var((str));
+    vs v;
+    string t;
+    while (getline((var), t, (ch)))
+    {
+        v.pb(t);
+    }
+    return v;
+}
+
+void err(istream_iterator<string> it) {}
+template <typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args)
+{
+    cout << *it << " = " << a << endl;
+    err(++it, args...);
+}
+// typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+// typedef trie<string,null_type,trie_string_access_traits<>,pat_trie_tag,trie_prefix_search_node_update> pbtrie;
+
+void file_i_o()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve(string digits, string output, int idx, vector<string> &ans, string mapping[])
+{
+    if (idx >= digits.length())
+    {
+        ans.push_back(output);
+        return;
+    }
+
+    int number = digits[idx] - '0';
+    string value = mapping[number];
+
+    for (int i = 0; i < value.length(); i++)
+    {
+        output.push_back(value[i]);
+        solve(digits, output, idx + 1, ans, mapping);
+        output.pop_back();
+    }
+}
+
+vector<string> letterCombinations(string digits)
+{
+    vector<string> ans;
+    if (digits.length() == 0)
+        return ans;
+    string output;
+    int idx = 0;
+    string mapping[10] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    solve(digits, output, idx, ans, mapping);
+    return ans;
+}
+
+int main(int argc, char const *argv[])
+{
+    clock_t begin = clock();
+    file_i_o();
+    string s;
+    cin >> s;
+    vector<string> ans = letterCombinations(s);
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << nl;
+    }
+    cout << nl;
+
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted In: " << double(end - begin) / CLOCKS_PER_SEC * 1000 << " ms";
+#endif
+    return 0;
+}
