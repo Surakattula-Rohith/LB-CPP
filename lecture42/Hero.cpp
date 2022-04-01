3b1d2be5c7acd4875069f00df5cb8e3ab483d4a9
Surakattula Rohith <srohith.charan@gmail.com>
2022-04-01 07:00:21 +0530

    01_04 06_00

--- lecture42/Hero.cpp
+++ lecture42/Hero.cpp
@@ -8,8 +8,27 @@ class Hero
     int health;
 
 public:
-    string name;
+    char *name;
     char level;
+    static int timeRequired;
+    Hero()
+    {
+        name = new char[100];
+        // cout << nl << "Constructor" << nl <<nl;
+    }
+    // Hero(int health){
+    // cout << "this value is " << this << nl ;
+    //     this->health = health;
+    // }
+    Hero(Hero &temp)
+    {
+        char *ch = new char[strlen(temp.name) + 1];
+        strcpy(ch, temp.name);
+
+        this->name = ch;
+        this->health = temp.health;
+        this->level = temp.level;
+    }
 
     void setHealth(int n)
     {
@@ -18,5 +37,33 @@ public:
     int getHealth()
     {
         return health;
+    }
+
+    void setLevel(char n)
+    {
+        level = n;
+    }
+    char getLevel()
+    {
+        return level;
+    }
+
+    void setName(char name[])
+    {
+        strcpy(this->name, name);
+    }
+
+    void print()
+    {
+        cout << name << " " << health << " " << level << nl;
+    }
+
+    static int random(){
+        return timeRequired ;
+    }
+
+    ~Hero()
+    {
+        cout << nl << "Destructor called !!" << nl;
     }
 };
--- lecture42/lecture42.cpp
+++ lecture42/lecture42.cpp
@@ -15,6 +15,8 @@ void file_i_o()
 #endif
 }
 
+int Hero::timeRequired = 10 ;
+
 int main(int argc, char const *argv[])
 {
     clock_t begin = clock();
@@ -24,32 +26,77 @@ int main(int argc, char const *argv[])
     cin >> n;
     cout << n << nl;
 
-    Hero h1;
-    cout << sizeof(h1) << nl;
+    // Hero h1;
+    // cout << sizeof(h1) << nl;
 
-    h1.name = "Ramesh";
-    h1.level = 'A';
-    h1.setHealth(70);
+    // h1.name = "Ramesh";
+    // h1.level = 'A';
+    // h1.setHealth(70);
 
-    cout << nl << h1.name << " " << h1.level << " " << h1.getHealth() << nl << nl ;
+    // cout << nl << h1.name << " " << h1.level << " " << h1.getHealth() << nl << nl;
 
-    Hero a ; 
-    Hero *b = new Hero ;
-    cout << sizeof(b) << nl ;
-    (*b).setHealth(100) ;
-    cout << (*b).getHealth() << nl ;
-    cout << b << nl <<nl ;
+    // Hero a;
+    // Hero *b = new Hero;
+    // cout << sizeof(b) << nl;
+    // (*b).setHealth(100);
+    // cout << (*b).getHealth() << nl;
+    // cout << b << nl << nl;
 
-    b->name = "Rohith" ;
-    b->level = 'S' ;
-    cout << b->name << nl ;
-    cout << b->level << nl ;
+    // b->name = "Rohith";
+    // b->level = 'S';
+    // cout << b->name << nl;
+    // cout << b->level << nl;
 
     cout << nl;
 
+    // Hero k(10);
+    // cout << k.getHealth() << nl;
+
+    Hero h3;
+    h3.setHealth(300) ;
+    h3.setLevel('F');
+    char name[7] = "Babbar" ;
+    h3.setName(name) ;
+
+    Hero h4 = h3;
+
+    h3.print();
+    h4.print();
+
+    cout << nl ;
+    h3.name[0] = 'G';
+
+    h3.print();
+    h4.print();
+    cout << nl ;
+    
+    // SHALLOW COPY
+
+    // Babbar 300 F
+    // Babbar 300 F
+
+    // Gabbar 300 F
+    // Gabbar 300 F
+
+    //DEEP COPY
+
+    // Babbar 300 F
+    // Babbar 300 F
+
+    // Gabbar 300 F
+    // Babbar 300 F
+
+    Hero *ab = new Hero() ;
+
+    cout << nl << ab->timeRequired << nl ;
+    delete ab ;
+
+    cout << Hero::timeRequired  << nl ;
+    cout << Hero::random() << nl  ;
+
 #ifndef ONLINE_JUDGE
     clock_t end = clock();
-    cout << "\n\nExecuted In: " << double(end - begin) / CLOCKS_PER_SEC * 1000 << " ms";
+    cout << "\n\nExecuted In: " << double(end - begin) / CLOCKS_PER_SEC * 1000 << " ms" << nl ;
 #endif
     return 0;
 }
